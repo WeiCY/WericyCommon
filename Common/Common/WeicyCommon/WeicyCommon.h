415cefe880f1643c702d873a357534f66e31a67c
weicy <weicy@citydo.com.cn>
2021-01-28 16:58:41 +0800

    fix

--- Common/Common/WeicyCommon/WeicyCommon.h
+++ Common/Common/WeicyCommon/WeicyCommon.h
@@ -11,7 +11,7 @@
 /**
  本工具库是我开发常用的一些类库
  参考YYKit、KJExtensionHandler等
- TODO: 新增UITextField、UITextView的长度限制、输入监听、placeholder  暂时使用JHInputLimit
+ TODO: 新增UITextView的长度限制、输入监听、placeholder 暂时使用JHInputLimit,
  TODO: 新增UIButton   新增点击范围扩大功能？
  TODO: 新增一行代码调用相机相册 单选 多选 保存相册
  TODO: 一行代码调用UM分享、系统自带分享
