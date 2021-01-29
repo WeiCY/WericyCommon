8d0a24652e53d867c3a008e74e749b6a78ce9578
weicy <weicy@citydo.com.cn>
2021-01-29 16:29:00 +0800

    fix

--- Common/Common/ViewController.m
+++ Common/Common/ViewController.m
@@ -29,22 +29,6 @@
 //    button.frame = CGRectMake(100, 100, 100, 100);
 //    [self.view addSubview:button];
 //    self.btn = button;
-    
-    UITextField *textfield = [[UITextField alloc] initWithFrame:CGRectMake(30, 150, 200, 40)];
-    textfield.backgroundColor = UIColor.whiteColor;
-    textfield.placeholder = @"ddddd";
-//    textfield.placeholderColor = UIColor.redColor;
-    textfield.font = [UIFont systemFontOfSize:12];
-    
-    textfield.placegolderFont = [UIFont boldSystemFontOfSize:19];
-    textfield.limitCount = 4;
-    [self.view addSubview:textfield];
-    
-    NSLog(@"%@",[WeicyFileTool weicy_listFilesInTmpDirectoryByDeep:YES]);
-    NSLog(@"%@",[UIApplication sharedApplication].tmpPath);
-    NSString *path = [NSString stringWithFormat:@"%@%@",[UIApplication sharedApplication].tmpPath,@"33"];
-    NSLog(@"%ld",[WeicyFileTool weicy_createDirectoryAtPath:path]);
-    NSLog(@"%@",[WeicyFileTool weicy_listFilesInTmpDirectoryByDeep:YES]);
     // Do any additional setup after loading the view.
 }
 
